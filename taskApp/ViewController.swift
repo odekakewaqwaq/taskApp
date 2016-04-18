d2239d0f641a2b322078beb6bcce33245deda7ec
永井伸枝 <odekakewaqwaq@gmail.com>
2016-04-19 06:12:23 +0900

    Initial Commit

---
+++ taskApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  taskApp
+//
+//  Created by 永井 伸枝 on 2016/04/19.
+//  Copyright © 2016年 nobue.nagai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
