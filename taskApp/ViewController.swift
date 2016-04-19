2b541d52eb0c07d3eb59f623de279c567573ee00
永井伸枝 <odekakewaqwaq@gmail.com>
2016-04-19 18:51:59 +0900

    SlideShowAppです

--- taskApp/ViewController.swift
+++ taskApp/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +20,17 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView : UITableView, numberOfRowsInsSection section:Int) -> Int{
+        return 0
+    }
+    
+    func tableView(tableView : UITableView, didSelectRowAtIndexPath indexpPath : NSIndexPath){
+    }
+    
+    func tableView(tableView: UITableView, editingStyleForRowAtIndexPath indexPath: NSIndexPath)-> UITableViewCellEditingStyle{
+        return UITableViewCellEditingStyle.Delete
     }
 
 
